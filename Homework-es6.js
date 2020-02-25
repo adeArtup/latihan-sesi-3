dd90bcc61b9eb13f7752367ee196524784f86e7b
artupAde <ade.lesmana@bni.co.id>
2020-02-25 15:25:44 +0700

    Add file homework es6

--- Homework-es6.js
+++ Homework-es6.js
@@ -7,3 +7,25 @@ class Shape {
         return x+""+y;
     }
 }
+
+class Rectangel extends Shape{
+    constructor(id,x,y,width,height){
+
+    }
+}
+
+class Circle extends Shape {
+    constructor(id,x,y,radius){
+
+    }
+}
+
+class Eye extends Circle{
+    constructor(id,x,y,color){
+
+    }
+
+    roll(rollAmount){
+        
+    }
+}
