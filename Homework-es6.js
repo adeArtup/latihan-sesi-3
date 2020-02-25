1b7eed0feed945ec4a07ce030198c3c8b932a468
artupAde <ade.lesmana@bni.co.id>
2020-02-25 16:06:37 +0700

    Update homework es6

--- Homework-es6.js
+++ Homework-es6.js
@@ -1,28 +1,45 @@
 class Shape {
-    constructor(id,x,y){
-
+    constructor(x,y){
+        this._id = 1;
+        this._x = x;
+        this._y = y;
     }
 
-    move(x, y){
-        return x+""+y;
+    move(){
+        return this._x +","+ this._y;
     }
 }
 
-class Rectangel extends Shape{
-    constructor(id,x,y,width,height){
+let Sh = new Shape(1,2);
+console.log(Sh.move());
 
+class Rectangel extends Shape{
+    constructor(x,y,width,height){
+        this.idr = 2;
+        this.xr = x;
+         this.yr = y;
+        this._w = width;
+        this._h = height;
     }
 }
 
 class Circle extends Shape {
-    constructor(id,x,y,radius){
+    constructor(x,y,radius){
+
+        this.idc = 3;
+        this.xc = x;
+         this.yc = y;
+        this._radius = radius;
 
     }
 }
 
 class Eye extends Circle{
-    constructor(id,x,y,color){
-
+    constructor(x,y,color){
+        this.ide = 4;
+        this.xe = x;
+         this.ye = y;
+        this._color = color;
     }
 
     roll(rollAmount){
